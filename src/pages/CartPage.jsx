3cf66427f8970b96d4f7c947f941dd0297e0b60e
Phan Ky Anh <phankyanh789@gmail.com>
2025-07-12 18:09:26 +0700

    Cập nhật nút callback cho CartPage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,9 +1,11 @@
 // import React, { useState } from "react";
 import { useCart } from "../contexts/CartContext";
+import { useNavigate } from "react-router-dom";
 import "../assets/styles/CartPage.css";
 
 function CartPage() {
   const { cartItems, addToCart, decreaseQuantity , removeFromCart } = useCart();
+  const navigate = useNavigate(); // ✅ hook điều hướng
 
   const handlePlus = (item) => {
     addToCart(item); // tăng thêm 1 sản phẩm (nó sẽ tự gộp trong context)
@@ -18,8 +20,16 @@ const handleMinus = (item) => {
       alert(`Đã mua sản phẩm: ${item.title}`);
   };
 
+   const handleGoBack = () => {
+    navigate(-1); // ✅ quay lại trang trước
+  };
+
   return (
     <div className="cart-container">
+      {/* ✅ Nút quay lại */}
+      <button className="btn btn-secondary mb-3" onClick={handleGoBack}>
+        <i className="bi bi-arrow-left me-2"></i> Quay lại
+      </button>
       <h1 className="title">Giỏ hàng của bạn</h1>
       {cartItems.length === 0 ? (
         <p>Không có sản phẩm nào trong giỏ.</p>

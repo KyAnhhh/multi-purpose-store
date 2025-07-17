ffb7132273ed5418f6dbdea02165b75389b21003
Phan Ky Anh <phankyanh789@gmail.com>
2025-07-17 11:31:31 +0700

    add loading skeleton for productlist

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,30 @@
 import React from "react";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import ProductItem from "./ProductItem";
 import "../assets/styles/ProductList.css"
 
-function ProductList({ products = [] , onAddToCart}) {
+function ProductList({ products , onAddToCart, loading}) {
+  if(loading){
+    return(
+      <div className="product-list">
+        {Array.from({ length: 10 }).map((_, index) => (
+          <div className="product-card skeleton-card" key={index}>
+            <Skeleton height={150} className="product-img-skeleton" />
+            <div className="product-content">
+              <Skeleton height={20} width={`80%`} />
+              <Skeleton height={20} width={`40%`} style={{ marginTop: 10 }} />
+              <Skeleton height={35} width={`100%`} style={{ marginTop: 15 }} />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
   return (
     <div className="product-list">
       {products.map((product) => (
-        <ProductItem key={product.id} product={product} onAddToCart={onAddToCart} />
+        <ProductItem key={product.id} product={product} loading = {loading} onAddToCart={onAddToCart} />
       ))}
     </div>
   );
